367a4701bc13303e82a616357e3b2a8b2365519f
Như Minh <nhuminh@mac.home>
2025-09-20 01:43:50 -0400

    90% done

--- main.js
+++ main.js
@@ -1,20 +1,59 @@
-const buyBtns = document.querySelectorAll('.js-buy-ticket')
-            const modal = document.querySelector('.js-modal')
-            const modalClose = document.querySelector('.js-modal-close')
-            const modalContainer = document.querySelector('.js-modal-container')
-            function showBuyTickets(){
-                modal.classList.add('open')
-            }
-            
-            function hideBuyTickets(){
-                modal.classList.remove('open')
-            }
-            for(const buyBtn of buyBtns){
-                buyBtn.addEventListener('click', showBuyTickets)
-            }
+// tour_places-list_buy-ticket-btn.js
+document.addEventListener('DOMContentLoaded', function () {            
+    const buyBtns = document.querySelectorAll('.js-buy-ticket');
+    const modal = document.querySelector('.js-modal');
+    const modalClose = document.querySelector('.js-modal-close');
+    const modalContainer = document.querySelector('.js-modal-container');
+    function showBuyTickets(){
+        modal.classList.add('open')
+    }
+    
+    function hideBuyTickets(){
+        modal.classList.remove('open')
+    }
+    for(const buyBtn of buyBtns){
+        buyBtn.addEventListener('click', showBuyTickets)
+    }
 
-            modalClose.addEventListener('click', hideBuyTickets)
-            modal.addEventListener('click', hideBuyTickets)
-            modalContainer.addEventListener('click', function (event){
-                event.stopPropagation()
-            })
+    modalClose.addEventListener('click', hideBuyTickets)
+    modal.addEventListener('click', hideBuyTickets)
+    modalContainer.addEventListener('click', function (event){
+        event.stopPropagation()
+    })
+});
+
+
+// map.js
+window.addEventListener("load", initMap);
+function initMap() {
+    let displayMap = document.querySelector("#map .display-map");
+    // Create a map to the The Band
+    let Headquarter = { lat: 40.7579787, lng: -73.9881229 };
+    let myMap = new google.maps.Map(displayMap, {
+        zoom: 11,
+        center: Headquarter,
+        fullscreenControl: false
+    });
+
+    // Add a marker for the Band
+    new google.maps.Marker({
+        position: Headquarter,
+        map: myMap,
+        title: "The Band's Headquarter is here"
+    });
+
+    
+    
+}
+function handleError(err) {
+    console.log("Geolocation error: " + err.message);
+    
+    let displayMap = document.querySelector("#map .display-map");
+    let errorImage = document.getElementById("geoErrorImage");
+    if (errorImage) {
+        errorImage.style.display = "block";
+    }
+    if (displayMap) {
+        displayMap.style.display = "none";
+    }
+}
